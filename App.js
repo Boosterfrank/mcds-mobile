5e650155478114eb3eee1e517839f4380c060ac7
Boosterfrank <francobetancourt@gmail.com>
2025-10-14 15:10:30 -0400

    line formatting

--- App.js
+++ App.js
@@ -21,16 +21,7 @@ import moment from 'moment';
 import * as Linking from 'expo-linking';
 import FormattedText from './components/FormattedText';
 import { db } from './firebaseConfig';
-import { 
-  collection, 
-  doc, 
-  getDoc,      // 👈 add this
-  setDoc, 
-  getDocs, 
-  query, 
-  orderBy, 
-  limit 
-} from 'firebase/firestore';
+import { collection, doc, getDoc, setDoc, getDocs, query, orderBy, limit } from 'firebase/firestore';
 
 // --- SVG Icon Definitions ---
 const homeIconXml = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8h5z"/></svg>`;
